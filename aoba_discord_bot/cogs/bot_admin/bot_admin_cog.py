63bbc410378b55e6dbe76434fcf7970d59852d7d
Douglas Carvalho <douglasc.dev@gmail.com>
2021-08-13 22:06:51 -0300

    Added an announce command and fixed a bug in status

--- aoba_discord_bot/cogs/bot_admin/bot_admin_cog.py
+++ aoba_discord_bot/cogs/bot_admin/bot_admin_cog.py
@@ -1,5 +1,7 @@
+from typing import List
+
 import discord
-from discord import Member
+from discord import Member, Guild, TextChannel
 from discord.ext import commands
 from discord.ext.commands import Context, Bot
 from sqlalchemy.orm import Session
@@ -30,11 +32,8 @@ class BotAdmin(
 
     @commands.is_owner()
     @commands.command(help="Change Aoba's status text")
-    async def status(self, ctx: Context, text: str = None):
-        if text:
-            await self.bot.change_presence(activity=discord.Game(text))
-            await ctx.send(f"My status was changed to `{text}`!")
-        else:
+    async def status(self, ctx: Context, *texts: str):
+        if not texts:
             member: Member = None
             for guild in self.bot.guilds:
                 member = guild.get_member(self.bot.user.id)
@@ -42,3 +41,42 @@ class BotAdmin(
                     break
             if member:
                 await ctx.send(member.activity)
+            return
+
+        status = " ".join(texts)
+        await self.bot.change_presence(activity=discord.Game(status))
+        await ctx.send(f"My status was changed to `{status}`!")
+
+    @commands.is_owner()
+    @commands.command(help="(UNTESTED)Make an announcement in every server")
+    async def announce(self, ctx: Context, *texts: str):
+        text = " ".join(texts)
+
+        await ctx.send(f"Announcing `{text}` in {len(self.bot.guilds)} servers.")
+
+        announcement_channels: List[TextChannel] = list()
+        guilds_with_no_announcement_channel: List[str] = list()
+
+        for guild in self.bot.guilds:
+            guild: Guild
+            announcement_channel: TextChannel = None
+
+            for channel in guild.text_channels:
+                if channel.is_news():
+                    announcement_channel = channel
+                    break
+
+            if not announcement_channel:
+                guilds_with_no_announcement_channel.append(guild.name)
+                continue
+
+            announcement_channels.append(announcement_channel)
+
+        if guilds_with_no_announcement_channel:
+            channels = ", ".join(guilds_with_no_announcement_channel)
+            await ctx.send(
+                f"Announcement channel was not found for guilds:\n > {channels}"
+            )
+
+        for channel in announcement_channels:
+            await channel.send(text)
